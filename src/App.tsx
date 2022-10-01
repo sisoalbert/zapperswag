e3b9b1915577fe67d500e51c8b345f53687a5a90
sisoalbert <sisoalbert@yahoo.com>
2022-10-01 19:23:01 +0200

    init comit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import People from "./components/People";
+import NavBar from "./components/NavBar";
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <People />
+    </div>
+  );
+}
